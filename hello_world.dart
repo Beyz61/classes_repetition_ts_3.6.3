cca6b2ee948b2ba593d9a1ce1c7ec7d2f19fd708
beyzanur <beyzanur.o@icloud.com>
2024-09-04 11:37:00 +0200

    add Hello World2

---
+++ hello_world.dart
@@ -0,0 +1,19 @@
+/* class HelloWorld {
+// Attributes
+  String name;
+
+// Constructor
+  HelloWorld(this.name);
+
+// Methode
+  void sayHello() {
+    print(" Hello world");
+  }
+}
+*/
+
+class Hi {
+  void printText() {
+    print("Hello, World!");
+  }
+}
